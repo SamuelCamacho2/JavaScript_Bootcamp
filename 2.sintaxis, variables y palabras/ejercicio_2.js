c99b8b00261d20b15f061c7dc48a9f10f965472b
SamuelCamacho2 <camacho_samuel29@hotmail.com>
2023-07-07 11:29:00 -0600

    segundo ejercicio

---
+++ 2.sintaxis, variables y palabras/ejercicio_2.js
@@ -0,0 +1,14 @@
+const informacion = [
+    'samuel camacho torres', 
+    22, 
+    true, 
+    fecha = new Date(2000, 10, 29), 
+    libro = {
+        titulo: "juegos de tronos cancion de hielo y fuego",
+        autor: "George R.R. martin",
+        fecha: "3 de abril 2014",
+        url: "https://www.amazon.com.mx/Juego-tronos-Canci%C3%B3n-Hielo-Fuego/dp/6073128835/ref=d_pd_sbs_sccl_2_1/138-8113707-8368248?pd_rd_w=TRdB1&content-id=amzn1.sym.bf51709a-d6bb-4474-b305-30d2f8a81b78&pf_rd_p=bf51709a-d6bb-4474-b305-30d2f8a81b78&pf_rd_r=HG929XYP7NK5SGBFN04W&pd_rd_wg=vBlFi&pd_rd_r=491b8f59-53b5-489b-a5bc-587f1dd3c6fa&pd_rd_i=6073128835&psc=1",
+    }
+    ]
+
+console.log(informacion[4])
