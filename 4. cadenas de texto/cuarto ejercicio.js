0c11c9bd5b266e934356c4c2ea910c3cec12ab57
SamuelCamacho2 <camacho_samuel29@hotmail.com>
2023-07-13 15:21:47 -0600

    cuarto ejercicio

---
+++ 4. cadenas de texto/cuarto ejercicio.js
@@ -0,0 +1,14 @@
+let nombre='Samuel'
+let apellido='Camacho Torres'
+
+let estudiante = nombre +" "+apellido
+let estudianteMay = estudiante.toUpperCase()
+let estudianteMin = estudiante.toLowerCase()
+
+let len_estudiante = estudiante.length
+let primerCaracter = estudiante.charAt(0)
+let ultimoCaracter = estudiante.charAt(estudiante.length - 1)
+let noEspacios = estudiante.replace(/\s+/g, '')
+let exites = estudiante.includes(nombre)
+
+console.log(exites)
